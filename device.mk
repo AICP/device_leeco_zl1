c9354b820f342edc98c8669395c7b9833a148fc7
mosimchah <mosimchah@gmail.com>
2017-05-12 12:20:59 -0400

    decommonize, and update the dalvik-heap and hwui-memory configs
    Change-Id: I3f3284740f3cd7a3e3916d5cb0b544df526990f6

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080

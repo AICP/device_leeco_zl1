b94b9446309abcf6a4d4936d2db0bd8741e5b750
mosimchah <mosimchah@gmail.com>
2017-09-16 22:26:41 -0400

    zl1: Update dalvik heap config from stock 28s
    Change-Id: I94687f37330265efbd609ea2286ce64d3a0a51fb

--- device.mk
+++ device.mk
@@ -34,10 +34,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapgrowthlimit=384m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
 #HWUI

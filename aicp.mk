3701a820bbd9811f3b3fa92e883e137ecd3d078e
mosimchah <mosimchah@gmail.com>
2017-09-16 22:51:47 -0400

    add back, and update the aicp.mk
    Change-Id: I53aa0aa4f3f0213438f33ac4a49947ac59e8e15b

---
+++ aicp.mk
@@ -0,0 +1,50 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from zl1 device
+$(call inherit-product, device/leeco/zl1/device.mk)
+
+# Inherit some common aicp stuff
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aicp_zl1
+PRODUCT_DEVICE := zl1
+PRODUCT_MANUFACTURER := LeEco
+PRODUCT_BRAND := LeEco
+
+PRODUCT_GMS_CLIENTID_BASE := android-leeco
+
+TARGET_VENDOR_PRODUCT_NAME := LePro3
+TARGET_VENDOR_DEVICE_NAME := le_zl1
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=le_zl1 PRODUCT_NAME=LePro3
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=LeEco/ZL1_NA/le_zl1:6.0.1/WEXNAOP5802101261S/letv01261206:user/release-keys \
+    PRIVATE_BUILD_DESC="le_zl1-user 6.0.1 WEXNAOP5802101261S eng.letv.20170126.120318 release-keys"
+
+TARGET_VENDOR := leeco
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk

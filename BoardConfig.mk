8b6cd9a9b7bb458fad95f02f93f82b9339db6634
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-02 12:27:02 +0100

    zl1: add support for LEX722 (zl0)
    Change-Id: I067c980ac1c2d965af3ab01fbd57ea2a24bcc8b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 # inherit from msm8996-common
 -include device/leeco/msm8996-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := le_zl1,zl1,lepro3,lex720,lex727,LEX720,LEX727
+TARGET_OTA_ASSERT_DEVICE := le_zl0,zl0,le_zl1,zl1,lepro3,lex720,lex727,LEX720,LEX727
 
 DEVICE_PATH := device/leeco/zl1
 
@@ -58,6 +58,9 @@ PRODUCT_BOOT_JARS += org.simalliance.openmobileapi
 
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy

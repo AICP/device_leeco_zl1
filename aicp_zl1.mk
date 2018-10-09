8b71248e5a3d6b0de57b94f37d8ce13f93f66275
mosimchah <mosimchah@gmail.com>
2018-10-09 19:35:31 -0400

    zl1:AICP-ify
    Change-Id: Iba576ef26aa0f4440bbd00355930c86704f4c8c9

--- aicp_zl1.mk
+++ aicp_zl1.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from zl1 device
 $(call inherit-product, device/leeco/zl1/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zl1
+PRODUCT_NAME := aicp_zl1
 PRODUCT_DEVICE := zl1
 PRODUCT_MANUFACTURER := LeEco
 PRODUCT_BRAND := LeEco

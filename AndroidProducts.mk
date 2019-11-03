LineageOS/android_device_leeco_zl1

zl1: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_zl1.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zl1-user \
+    lineage_zl1-userdebug \
+    lineage_zl1-eng

AICP/device_leeco_zl1

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_zl1.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_zl1-user \
+    aicp_zl1-userdebug \
+    aicp_zl1-eng
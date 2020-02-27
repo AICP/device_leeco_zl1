d2ec7ddd8f42b2c8098221efa4ecea811486faaa
mosimchah <mosimchah@gmail.com>
2020-02-27 10:41:02 -0500

    zl1: Import biometrics + fingerprint wrapper from le2
    Change-Id: Id7e38af48e0b5b44e827e5dd8d4bdf9d2091e0bd

--- fingerprint/gdxbiometrics/Android.mk
+++ fingerprint/gdxbiometrics/Android.mk
@@ -1,12 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service.leeco_zl1
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.leeco_zl1.rc
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.leeco_zl1
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.leeco_zl1.rc
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
-
-LOCAL_VENDOR_MODULE := true
-
 LOCAL_SRC_FILES := \
     BiometricsFingerprint.cpp \
     service.cpp \
@@ -26,7 +24,6 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     libhwbinder \
     libkeystore_aidl \
-    libkeystore_binder \
     android.hardware.biometrics.fingerprint@2.1
 
 include $(BUILD_EXECUTABLE)
